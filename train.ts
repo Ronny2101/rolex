04423b1ea9a9cd10c991a38eae357a3826b77a0d
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-06-05 00:58:06 +0900

    feat: install Task Zk

--- train.ts
+++ train.ts
@@ -1,17 +1,26 @@
 //Task ZJ
 
-function reduceNestedArray(array: any[]) {
-    let son = 0;
+// function reduceNestedArray(array: any[]) {
+//     let son = 0;
 
-    for(const val of array) {
-        son += Array.isArray(val) 
-        ? reduceNestedArray(val) 
-        : val;
-    }
+//     for(const val of array) {
+//         son += Array.isArray(val) 
+//         ? reduceNestedArray(val) 
+//         : val;
+//     }
 
-    return son;
+//     return son;
+// }
+
+// console.log(reduceNestedArray([1, [1, 2, [4]]]));
+
+
+//Task ZK
+
+function printNumbers(a: number = 1) {
+  if (a > 5) return;
+    console.log(a);
+    setTimeout(() => printNumbers(a + 1), 1000);
 }
 
-console.log(reduceNestedArray([1, [1, 2, [4]]]));
-
-/*"@types/graphql": "^14.2.3",*/
+printNumbers();
