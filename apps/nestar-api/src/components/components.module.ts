930bd0c63910d04db3324f6e7dba12a3b1b2f1bc
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-06-05 23:25:29 +0900

    feat: create member resolver & service model

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
-import { ComentModule } from './coment/coment.module';
+import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/like.module';
 import { ViewModule } from './view/view.module';
 import { FollowModule } from './follow/follow.module';
 import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule, AuthModule, ComentModule, LikeModule, ViewModule, FollowModule, BoardArticleModule]
+  imports: [
+    MemberModule,  
+    AuthModule, 
+    PropertyModule, 
+    BoardArticleModule, 
+    LikeModule, 
+    ViewModule, 
+    CommentModule, 
+    FollowModule,
+  ],
 })
 export class ComponentsModule {}
