f7489ed6af76f097551694b5271ff65ace961a56
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-06-27 06:31:05 +0900

    feat: develop likeTargetMember GraphQl api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -10,6 +10,9 @@ import { MemberUpdate } from '../../libs/dto/member/member.ubdate';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { LikeService } from '../like/like.service';
 
 @Injectable()
 export class MemberService {
@@ -17,6 +20,7 @@ export class MemberService {
     constructor(@InjectModel('Member') private readonly memberModel: Model<Member>, 
     private authService: AuthService,
     private viewService: ViewService,
+    private likeService: LikeService,
 ) {}
 
     public async signup(input: MemberInput): Promise<Member> {
@@ -92,31 +96,49 @@ export class MemberService {
         return targetMember;
     }
 
-    public async getAgents(memberId: ObjectId, input: AgentInquiry): Promise<Members>
- {
-    const { text } = input.search;
-    const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE };
-    const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+    public async getAgents(memberId: ObjectId, input: AgentInquiry): Promise<Members> {
+        const { text } = input.search;
+        const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE };
+        const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-    if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
-    console.log('match:', match);
+        if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
+        console.log('match:', match);
 
-    const result = await this.memberModel
-     .aggregate([
-        { $match: match },
-        { $sort: sort },
-        {
-            $facet: {
-                list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
-                metaCounter: [{ $count: 'total' }],
+        const result = await this.memberModel
+        .aggregate([
+            { $match: match },
+            { $sort: sort },
+            {
+                $facet: {
+                    list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+                    metaCounter: [{ $count: 'total' }],
+                },
             },
-        },
-     ])
-     .exec();
-     if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-     return  result[0];
- }
+        ])
+        .exec();
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        return  result[0];
+    }
 
+
+    public async likeTargetMember(memberId: ObjectId, likeRefId: ObjectId): Promise<Member> {
+        const target: Member = await this.memberModel.findOne({ _id: likeRefId, memberStatus: MemberStatus.ACTIVE}).exec();
+        if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        const input: LikeInput = {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            likeGroup: LikeGroup.MEMBER,
+        };
+
+        // LIKE TOGGLE via Like modules
+        const modifier: number =  await this.likeService.toggleLike(input);
+        const result = await this.memberStatsEditor({ _id: likeRefId, targetKey: 'memberLikes', modifier: modifier });
+
+        if(!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+        return result;
+    }
+    
 
     public async getAllMembersByAdmin(input: MembersInquiry): Promise<Members> {
         const { memberStatus, memberType, text } = input.search;
