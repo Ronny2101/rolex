b69437ec395cef4cc4313f57d4ffb4579094efd7
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-06-30 17:30:21 +0900

    feat: develop getFavorites GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -5,7 +5,7 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
-import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, OrdinaryInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
@@ -66,6 +66,17 @@ export class PropertyResolver {
           return await this.propertyService.getProperties(memberId, input);
      }
 
+     @UseGuards(AuthGuard)
+     @Query((returns) => Properties)
+     public async getFavorites(
+          @Args('input') input: OrdinaryInquiry,
+          @AuthMember('_id') memberId: ObjectId,
+     ): Promise<Properties> {
+          console.log('Query: getProperties');
+          return await this.propertyService.getFavorites(memberId, input);
+     }
+
+     
      @Roles(MemberType.AGENT)
      @UseGuards(RolesGuard)
      @Query((returns) => Properties)
