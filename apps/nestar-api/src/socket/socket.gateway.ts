362f627b5d6dca5682890d9f343f03a882329624
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-07-31 00:13:57 +0900

    fix: modify WebSocket Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -1,16 +1,22 @@
 import { Logger } from '@nestjs/common';
+import { AuthService } from "..//components/auth/auth.service";
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server} from 'ws';
 import * as WebSocket from "ws";
+import { Member } from '../libs/dto/member/member';
+import * as url from 'url'; 
 
 interface MessagePayload {
   event: string;
   text: string;
+  memberData: Member;
 }
 
 interface InfoPayload {
   event: string;
   totalClients: number;
+  memberData: Member;
+  action: string;
 }
 
 @WebSocketGateway({ transports: ['websocket'], secure: false })
@@ -18,6 +24,11 @@ export class SocketGateway implements OnGatewayInit{
 
   private logger: Logger = new Logger('SocketEventsGateway');
   private summaryClient: number = 0;
+  private clientsAuthMap = new Map<WebSocket, Member>();
+  private messagesList: MessagePayload[] = [];
+
+
+  constructor(private authService: AuthService) {}
 
   @WebSocketServer()
   server: Server;
@@ -26,25 +37,50 @@ export class SocketGateway implements OnGatewayInit{
     this.logger.verbose(`WebSocket Server InitIalized total [${this.summaryClient}]`);
   }
 
-  handleConnection(client: WebSocket, ...args: any[]) {
+  private async retrieveAuth(req: any): Promise<Member> {
+    try {
+      const parseUrl = url.parse(req.url, true);
+      const { token } = parseUrl.query;
+      console.log('token:', token);
+      return await this.authService.verifyToken(token as string)
+    }catch (err) {
+      return null;
+    }
+  }
+
+  public async handleConnection(client: WebSocket, req: any) {
+    const authMember = await this.retrieveAuth(req);
+    console.log('authMember:', authMember);
     this.summaryClient++;
-    this.logger.verbose(`Connection &  total [${this.summaryClient}]`);
+    this.clientsAuthMap.set(client, authMember);
+
+    const clientNick: string = authMember?.memberNick ?? "Guest";
+    this.logger.verbose(`Connection [${clientNick}]&  total [${this.summaryClient}]`);
 
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action: 'joined',
     };
     this.emitMessage(infoMsg);
+    client.send(JSON.stringify({event: 'getMessages', list: this.messagesList }));
   }
 
 
-  handleDisconnect(client: WebSocket) {
+  public async handleDisconnect(client: WebSocket) {
+    const authMember = this.clientsAuthMap.get(client);
     this.summaryClient--;
-    this.logger.verbose(`Disconnection &  total [${this.summaryClient}]`);
+    this.clientsAuthMap.delete(client);
+
+    const clientNick: string = authMember?.memberNick ?? 'Guest';
+    this.logger.verbose(`Disconnection [${clientNick}]&  total [${this.summaryClient}]`);
     
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action: 'left',
     };
     this.boardcastMessage(client, infoMsg);
   }
@@ -52,10 +88,16 @@ export class SocketGateway implements OnGatewayInit{
 
   @SubscribeMessage('message')
   public async handleMessage(client: any, payload: any): Promise <void> {
-    const newMessage: MessagePayload = { event: 'message', text: payload};
+    const authMember = this.clientsAuthMap.get(client)
+    const newMessage: MessagePayload = { event: 'message', text: payload, memberData: authMember};
 
-    this.logger.verbose(`NEW MESSAGE: ${payload}`);
-    this.emitMessage(newMessage)
+    const clientNick: string = authMember?.memberNick ?? 'Guest';
+    this.logger.verbose(`NEW MESSAGE [${clientNick}]: ${payload}`);
+
+    this.messagesList.push(newMessage);
+    if (this.messagesList.length > 5) this.messagesList.splice(0, this.messagesList.length - 5);
+
+    this.emitMessage(newMessage);
   }
 
   private boardcastMessage(sender: WebSocket, message: InfoPayload | MessagePayload) {
